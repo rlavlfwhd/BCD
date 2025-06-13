b47e456e7526ebd159cd7c2732d491e08d6f864c
fightjinwoo <2021137004@g.shingu.ac.kr>
2025-06-13 13:38:10 +0900

    [Refactor] 스토리 퍼즐 연결 최종 완료
    현재 빌드파일 STOV제출

--- Assets/Test Scean/Script/SceneChanger.cs
+++ Assets/Test Scean/Script/SceneChanger.cs
@@ -3,13 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    // 전환할 씬 이름
-    [SerializeField] private string sceneToLoad = "GardenScene";
+   
 
     // 마우스로 클릭했을 때
     private void OnMouseDown()
     {
-        // 씬 전환
-        SceneManager.LoadScene(sceneToLoad);
+
+        StartCoroutine(FadeManager.Instance.FadeToChoiceScene("PFlowerScene"));
     }
 }
